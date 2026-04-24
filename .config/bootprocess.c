6b113b2323b316b904356a0fc442893ff154138a
Slabs_createor <249469506+shrijiithmahesh-eng@users.noreply.github.com>
2026-04-24 16:41:56 +0530

    Create bootprocess.c

---
+++ .config/bootprocess.c
@@ -0,0 +1,14 @@
+#include "Logon.bin"
+#include <stdlib.h>
+_start();
+void usetconf() {
+  system("echo ");
+}
+
+void boglog() {
+  system("./.config/Teoth.sh");
+}
+int main() {
+  goto boglog();
+  return 0;
+}
